ae10e985ef51ffa0cc5dca0afc7c0edc8a9db39f
ehmatthes <eric@example.com>
2021-03-14 20:29:58 -0700

    add relationships

--- layoverguru-backend/app/serializers/activity_serializer.rb
+++ layoverguru-backend/app/serializers/activity_serializer.rb
@@ -1,3 +1,4 @@
 class ActivitySerializer < ActiveModel::Serializer
   attributes :id, :name, :description
+  belongs_to :airport
 end
--- layoverguru-backend/app/serializers/airport_serializer.rb
+++ layoverguru-backend/app/serializers/airport_serializer.rb
@@ -1,3 +1,4 @@
 class AirportSerializer < ActiveModel::Serializer
   attributes :id, :name, :code, :visited
+  has_many :activities
 end
